ab5a5069ad44db84bad07594c3d9784f41a50a86
mcLYX <1395365273@qq.com>
2026-08-18 23:21:28 +0800

    p0-1

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.poluxis.app',
+  appName: 'Project:Poluxis',
+  webDir: 'dist'
+};
+
+export default config;
